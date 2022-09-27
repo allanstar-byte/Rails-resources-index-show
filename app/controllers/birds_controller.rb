4148297de2a6fb374a27c62ad31267728aa4ab20
allanstar-byte <allanmutisya263@gmail.com>
2022-09-27 11:57:36 +0300

    Routing

---
+++ app/controllers/birds_controller.rb
@@ -0,0 +1,37 @@
+class BirdsController < ApplicationController
+    # GET birds
+    def index
+        birds = Bird.all
+        render json: birds
+    end
+
+    # GET birds/:id
+    def show
+        bird = Bird.find(id: params[:id])
+        if bird
+            render json: bird
+        else
+            render json: { error: 'bird not found' }, status: :not found_found
+        end
+    end
+end
+
+# class BirdsController < ApplicationController
+
+#     # GET /birds
+#     def index
+#       birds = Bird.all
+#       render json: birds
+#     end
+  
+#     # GET /birds/:id
+#     def show
+#       bird = Bird.find_by(id: params[:id])
+#       if bird
+#         render json: bird
+#       else
+#         render json: { error: "Bird not found" }, status: :not_found
+#       end
+#     end
+  
+#   end
